4e6c764f83e4fef7e42aa91006052e44d4fafe1a
HARISH KUMAR <119542182+Mmmut2025@users.noreply.github.com>
2025-01-25 23:33:45 +0530

    Add files via upload

---
+++ DAY-3/Percentage.java
@@ -0,0 +1,10 @@
+class Percentage {
+    public static void main(String args[]) {
+        
+        int total = 500;
+        int obtained = 434;
+	double per = (obtained * 100) / total;
+
+        System.out.println("percentage: " + per + "%");
+    }
+}
