b40e7f66d0b165123279e38416c19f32cb335ad2
HARISH KUMAR <119542182+Mmmut2025@users.noreply.github.com>
2025-01-25 17:57:26 +0530

    Add files via upload

---
+++ DAY-2/Main.java
@@ -0,0 +1,14 @@
+class Main
+{
+	public static void main(String args[])
+	{
+		// Creating an Object of "MethodDemo" class
+
+		MethodDemo md1 = new MethodDemo();
+		md1.a = 10;
+		md1.b = 20;
+		
+		md1.add();
+		md1.sub();
+	}
+}
---
+++ DAY-2/MethodDemo.java
@@ -0,0 +1,13 @@
+class MethodDemo
+{	
+	int a, b;
+	void add()
+	{
+		System.out.println("Sum of A and B: " + (a + b));
+	}
+	
+	void sub()
+	{
+		System.out.println("Difference of A and B: " + (a - b));
+	}
+}
